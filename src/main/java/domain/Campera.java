8a72105883d65cfdf955c3ec23ff5b501eebe7a8
Damuchi99 <damuchi99@gmail.com>
2020-04-29 17:05:44 -0300

    Modifico telas

--- src/main/java/domain/Campera.java
+++ src/main/java/domain/Campera.java
@@ -4,6 +4,7 @@ public class Campera extends Tipo{
 	public Campera(){
 		this.categoria = Categoria.SUPERIOR;
 		this.telasPosibles.add(new Algodon());
+		this.telasPosibles.add(new Cuero());
 		this.telasPosibles.add(new Nylon());
 		this.telasPosibles.add(new Seda());
 		this.telasPosibles.add(new Poliester());
